b480306702bbbb798bcd24ca470677d6f54e80bf
Jawann <9100510+jawanng@users.noreply.github.com>
2019-05-07 21:55:33 -0400

    Added comments to work log.  Also fixed a bug
    Had a bug when editing a title.  It would edit the name instead

--- work_log.py
+++ work_log.py
@@ -5,6 +5,7 @@ db = SqliteDatabase('tasks.db')
 
 
 class Task(Model):
+    """Task model with name, date, title, task_time, and notes."""
     date = DateField()
     name = CharField(max_length=70)
     title = CharField(max_length=255)
@@ -15,6 +16,7 @@ class Task(Model):
         database = db
 
     def __str__(self):
+        """Formatting for the Task"""
         return "Name: {}\nDate: {}\nTitle: {}\nTime Spent: {}\nNotes: {}\n".format(self.name,
                                                                                    self.date.strftime('%m/%d/%Y'),
                                                                                    self.title, self.task_time,
@@ -28,6 +30,15 @@ class Task(Model):
 
 
 def search_results():
+    """
+    Provides the search results.
+
+    This takes the option the user selected and calls the appropriate search.
+
+    Returns:
+    list: A list of the search results or None if the user wants to return to the main menu
+
+    """
     new_option = Common.search_menu()
     if new_option == 'a':
         list_employees()
@@ -44,6 +55,11 @@ def search_results():
 
 
 def search_sequence():
+    """
+    Gives a message if it receives and empty results array.
+    Calls Common.print_result_array with the current index starting with zero
+    Based on the user input, either call again with a different index number, edit, or delete the task.
+    """
     idx = 0
     results = search_results()
     if not results:
@@ -69,11 +85,13 @@ def search_sequence():
 
 
 def initialize():
+    """Initialize the database."""
     db.connect()
     db.create_tables([Task], safe=True)
 
 
 def add_task_menu():
+    """Sequence for adding a task."""
     name = input("Name of who worked on the task: ")
     Common.clear()
     date = Common.get_date()
@@ -89,11 +107,14 @@ def add_task_menu():
 
 
 def add_task(task_entry):
+    """Call to the datebase to create a task."""
     Task.create(date=task_entry[0], name=task_entry[1], title=task_entry[2], task_time=task_entry[3],
                 notes=task_entry[4])
 
 
 def edit_task(task):
+    """Takes a task to edit, queries the user or what to edit, perform the edit requested,
+    and save the task to the database."""
     while 1:
         Common.clear()
         print("What would you like to edit?")
@@ -102,7 +123,7 @@ def edit_task(task):
             task.name = input("Name of who worked on the task: ")
             break
         elif option.lower() == "title":
-            task.name = input("What is the title of the task: ")
+            task.title = input("What is the title of the task: ")
             break
         elif option.lower() == "date":
             task.date = Common.get_date()
@@ -119,6 +140,7 @@ def edit_task(task):
 
 
 def list_employees():
+    """When search by name, this provides a list of names available."""
     Common.clear()
     results = Task.select(Task.name).distinct()
     print("Employee(s)\n-----------")
@@ -128,6 +150,7 @@ def list_employees():
 
 
 def list_dates():
+    """When searching by date, this provides a list of dates available"""
     Common.clear()
     results = Task.select(Task.date).distinct()
     print("Date(s)\n-------")
@@ -138,14 +161,18 @@ def list_dates():
 
 
 def delete_task(task):
+    """Tasks a task as input and deletes it"""
     task.delete_instance()
 
 
 def search_employee(text):
+    """Takes a name or part of a name and return any hits."""
     return Task.select().where(Task.name.contains(text))
 
 
 def search_date():
+    """If the user wants an exact date, query the database for that date and return the results.
+    Otherwise, query the database for the range of dates specified and return the results."""
     option = Common.get_date_option()
     if option == 'E':
         list_dates()
@@ -158,14 +185,17 @@ def search_date():
 
 
 def search_duration(duration):
+    """Takes a number as input and returns any entries whose task_duration matches that number."""
     return Task.select().where(Task.task_time == duration)
 
 
 def text_search(text):
+    """Takes text as input and returns any records where the title or notes contains the text."""
     return Task.select().where(Task.title.contains(text) | Task.notes.contains(text))
 
 
 if __name__ == "__main__":
+    """Starts the task sequence."""
     initialize()
 
     while 1:
