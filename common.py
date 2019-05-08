ad512e49eaa9818b11464cd644662fd4e350986e
Jawann <9100510+jawanng@users.noreply.github.com>
2019-05-07 21:54:26 -0400

    Added comments to Common

--- common.py
+++ common.py
@@ -3,9 +3,16 @@ from os import system, name
 
 
 class Common:
+    """
+    Helper functions for the Task class.
+
+    All are class methods
+
+    """
 
     @classmethod
     def clear(cls):
+        """Needed because some people still use Windows :)"""
         if name == 'nt':
             _ = system('cls')
         else:
@@ -13,6 +20,15 @@ class Common:
 
     @classmethod
     def get_date(cls, intro_message="Date of the task"):
+        """
+        This prompts the user for a date and properly formats it
+
+        Parameters:
+        arg1 (text): Optional message to display to the user
+
+        Returns:
+        DateTime: Date entered by the user
+        """
         while 1:
             Common.clear()
             print(intro_message)
@@ -31,6 +47,7 @@ class Common:
 
     @classmethod
     def get_minutes(cls):
+        """Method to validate the input for length of task.  Returns (int) minutes"""
         while 1:
             Common.clear()
             try:
@@ -47,6 +64,7 @@ class Common:
 
     @classmethod
     def get_date_option(cls):
+        """Method to ask user if they want to use exact date or range of dates.  Returns (E or R)"""
         while 1:
             Common.clear()
             response = input("E) Exact date\nR) Range of dates\n> ")
@@ -58,6 +76,7 @@ class Common:
 
     @classmethod
     def main_message(cls):
+        """Gives the option of adding a new entry or searching existing entries. Returns (a, b, or c)"""
         Common.clear()
         while 1:
             option = input('''
@@ -75,6 +94,7 @@ class Common:
 
     @classmethod
     def search_menu(cls):
+        """Menu of search options. Returns (a, b, c, d, or e)"""
         Common.clear()
         while 1:
             option = input('''
@@ -94,12 +114,39 @@ class Common:
 
     @classmethod
     def print_result(cls, results, entry, total_num):
+        """
+        Formats and prints the results of the various searches
+
+        This clears the screen. Prints the entry. Then let's the user know that this is x of y entries.
+
+        Parameters:
+        arg1 (list): A list of search results
+        arg2 (int): The index of the current entry to display
+        arg3 (int): The total number of entries in the results array
+
+        Returns:
+        N/A
+
+        """
         Common.clear()
         print(results[entry])
         print("\nResult {} of {}\n".format(entry+1, total_num))
 
     @classmethod
     def print_result_array(cls, results, idx=0):
+        """
+        Prints the results along with a navigation menu.
+
+        The navigation menu has options to go forward, backward, edit or delete and entry.
+
+        Parameters:
+        arg1 (list): A list of search results
+        arg2 (int): An index of the current result.
+
+        Returns:
+        String: The users choice of action (Previous, Next, Delete, Edit, Return to Main Menu)
+
+        """
         total = len(results)
         while 1:
             Common.print_result(results, idx, total)
